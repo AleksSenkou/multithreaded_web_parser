e678213aad6cae4b7a093db00eefa3c3e31107ec
alekssenkou <aleksey.senkou@gmail.com>
2016-08-09 15:28:18 +0300

    Get urls from urls.txt file

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+ruby '2.2.4'
+
+gem 'pry', '~> 0.10.3'
+gem 'colorize', '~> 0.7.3'
+gem 'concurrent-ruby', require: 'concurrent'
--- website_searcher.rb
+++ website_searcher.rb
@@ -1,9 +1,26 @@
 require 'pry'
+require 'concurrent'
+require 'open-uri'
+require 'csv'
 
 class WebsiteSearcher
+  attr_reader :urls
+
   def initialize(file_with_urls_info = 'urls.txt')
-    @urls = file_with_urls_info
+    @urls_file = file_with_urls_info
+  end
+
+  def get_urls_from_file
+    # add check
+    @urls = CSV.read(@urls_file, headers: true)['URL']
   end
 end
 
-searcher = WebsiteSearcher.new('urls.txt')
+searcher = WebsiteSearcher.new
+searcher.get_urls_from_file
+
+pool = Concurrent::ThreadPoolExecutor.new(
+  min_threads: 1,
+  max_threads: 20,
+  max_queue: 10
+)
